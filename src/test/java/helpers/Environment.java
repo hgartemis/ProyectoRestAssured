62ce2615fc29352fcf673c7afd51fd177582768c
Humberto <hgartemis@gmail.com>
2021-05-22 19:03:19 -0500

    Nueva version usando factory 1

--- src/test/java/helpers/Environment.java
+++ src/test/java/helpers/Environment.java
@@ -4,7 +4,7 @@ public class Environment {
 
     public static String protocol="http://";
     public static String host="todo.ly";
-    public static String user="jb50@jb50.com";
+    public static String user="";
     public static String password="12345";
 
 }
